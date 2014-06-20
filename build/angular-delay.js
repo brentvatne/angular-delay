84a2314564aa444a3034e286776e2b67874a1086
Brent Vatne <brent.vatne@madriska.com>
2014-06-19 22:41:10 -0700

    Actually build it ..

---
+++ build/angular-delay.js
@@ -0,0 +1,38 @@
+(function(window, angular, undefined) {'use strict';
+
+angular.module('ngDelay', [])
+
+.directive('ngDelay', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        scope: true,
+        compile: function (element, attributes) {
+            var expression = attributes['ngChange'];
+            if (!expression)
+                return;
+
+            var ngModel = attributes['ngModel'];
+            if (ngModel) attributes['ngModel'] = '$parent.' + ngModel;
+            attributes['ngChange'] = '$$delay.execute()';
+
+            return {
+                post: function (scope, element, attributes) {
+                    scope.$$delay = {
+                        expression: expression,
+                        delay: scope.$eval(attributes['ngDelay']),
+                        execute: function () {
+                            var state = scope.$$delay;
+                            state.then = Date.now();
+                            $timeout(function () {
+                                if (Date.now() - state.then >= state.delay)
+                                    scope.$parent.$eval(expression);
+                            }, state.delay);
+                        }
+                    };
+                }
+            }
+        }
+    };
+}]);
+
+})(window, window.angular);
